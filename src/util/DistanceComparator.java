31e772ec4c4551481d349c69e721aebc3cdcb0dc
MaNOk06 <marthanaaokaileyafful@gmail.com>
2025-11-20 11:36:28 +0000

    Created file structure

---
+++ src/util/DistanceComparator.java
@@ -0,0 +1,5 @@
+package util;
+
+public class DistanceComparator {
+    // Logic to sort by proximity
+}
---
+++ src/util/SearchFilter.java
@@ -0,0 +1,5 @@
+package util;
+
+public class SearchFilter {
+    // Interface for polymorphism
+}
