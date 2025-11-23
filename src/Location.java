92a5e793d0404d976ec8a03621916999062569cd
sergi-e <ishserge16@gmail.com>
2025-11-23 12:49:16 +0000

    Updated Location class

--- src/Location.java
+++ src/Location.java
@@ -1,3 +1,37 @@
 public class Location {
-    
+    // name of the place
+    private String placeName;
+    // x coordinate
+    private int x;
+    // y coordinate
+    private int y;
+
+    // constructor to set the values
+    public Location(String placeName, int x, int y) {
+        this.placeName = placeName;
+        this.x = x;
+        this.y = y;
+    }
+
+    // returns the place name
+    public String getPlaceName() {
+        return placeName;
+    }
+
+    // returns x value
+    public int getX() {
+        return x;
+    }
+
+    // returns y value
+    public int getY() {
+        return y;
+    }
+
+    // calculates the distance between this location and another one
+    public double computeDistance(Location other) {
+        int dx = this.x - other.x;
+        int dy = this.y - other.y;
+        return Math.sqrt(dx * dx + dy * dy);
+    }
 }
